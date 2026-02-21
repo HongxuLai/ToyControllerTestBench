3679814b85bfee4768d9d572509ead70db9a9679
Hongxu Lai <hongxu.lai66@gmail.com>
2026-02-21 16:53:57 -0500

    Add core method reconcileOnce to make runningPods converge up or down.

--- src/tctb/controller/ToyController.java
+++ src/tctb/controller/ToyController.java
@@ -1,4 +1,19 @@
 package tctb.controller;
 
+import tctb.model.ActualState;
+import tctb.model.DesiredState;
+
+// converge the desiredState and actualState
 public class ToyController {
+    public void reconcileOnce(DesiredState desired, ActualState actual){
+        int desiredReplicas = desired.getReplicas();
+        int actualRunning = actual.getRunningPods();
+
+        if(desiredReplicas > actualRunning){
+            actual.setRunningPods(actualRunning + 1);
+        }
+        else if(desiredReplicas < actualRunning){
+            actual.setRunningPods(actualRunning - 1);
+        }
+    }
 }
