20c01a0a934dea10ec71a3b15f2b643c4b6ece3a
Hongxu Lai <hongxu.lai66@gmail.com>
2026-02-21 16:52:42 -0500

    add ActualState and DesiredState to store desired state and actual state

--- src/tctb/model/ActualState.java
+++ src/tctb/model/ActualState.java
@@ -1,4 +1,26 @@
 package tctb.model;
 
+// set actual runningPod >+ 0
+
 public class ActualState {
+    private int runningPods;
+
+    public ActualState(int runningPods) {
+        if (runningPods < 0) {
+            throw new IllegalArgumentException("runningPods must be >= 0");
+        }
+        this.runningPods = runningPods;
+    }
+
+    public int getRunningPods() {
+        return runningPods;
+    }
+
+    public void setRunningPods(int runningPods) {
+        if (runningPods < 0) {
+            throw new IllegalArgumentException("runningPods must be >= 0");
+        }
+        this.runningPods = runningPods;
+    }
+
 }
--- src/tctb/model/DesiredState.java
+++ src/tctb/model/DesiredState.java
@@ -1,4 +1,25 @@
 package tctb.model;
 
+// set the desired replicas >= 0
+
 public class DesiredState {
+    private int replicas;
+
+    public DesiredState(int replicas){
+        if (replicas < 0){
+            throw new IllegalArgumentException("replicas must >= 0");
+        }
+        this.replicas = replicas;
+    }
+
+    public int getReplicas(){
+        return replicas;
+    }
+
+    public void setReplicas(int replicas){
+        if (replicas < 0){
+            throw new IllegalArgumentException("replicas must >= 0");
+        }
+        this.replicas = replicas;
+    }
 }
